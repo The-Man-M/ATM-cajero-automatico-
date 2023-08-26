c99b204445587e6d01d6a9e563b3f25721d80280
Carlos Manuel <Carlos15ros@outlook.es>
2023-08-26 17:42:30 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ATM__cajero_automatico_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static int pepe(double retiro)
+        {
+            if (retiro > 0 && retiro <= 15)
+            {
+                return 0;
+            }
+            else if (retiro > 15 && retiro <= 50)
+            {
+                return 1;
+            }
+            else if (retiro > 50 && retiro <= 100)
+            {
+                return 2;
+            }
+            else if (retiro > 100)
+            {
+                return 3;
+            }
+            else { return 4; }
+        }
+        static void Billetes(int i)
+        {
+            int Billetes_de_uno = 0;
+            int Billetes_de_cinco = 0;
+            int Billetes_de_diez = 0;
+            int Billetes_de_veinte = 0;
+            int Billetes_de_cincuanta = 0;
+            switch (i)
+            {
+                case 0:
+
+
+
+
+                    break;
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                default: break;
+
+            }
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM__cajero_automatico_
+{
+    internal class Usuario
+    {
+        string Nombre_de_usuario = "";
+        string DUI = "";
+        string Contraseña = "";
+        double Saldo = 0;
+        bool cuenta_creada = false;
+
+        public void Crear_cuenta()
+        {
+            if (this.cuenta_creada)
+            {
+
+                Console.WriteLine("Cuenta ya creada");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese el nombre de usuario\n");
+                this.Nombre_de_usuario = Console.ReadLine();
+                Console.WriteLine("Ingrese numero de DUI\n");
+                this.DUI = Console.ReadLine();
+                Console.WriteLine("Ingrese su contraseña\n");
+                this.Contraseña = Console.ReadLine();
+                Console.WriteLine("Ingrese saldo inicial\n");
+                while (!double.TryParse(Console.ReadLine(), out Saldo) || Saldo< 0)//validamos que no ingrese números negativos ni caracter
+                {
+                    Console.WriteLine("Ingrese saldo valido\nIngrese saldo inicial\n");
+                }
+
+                cuenta_creada = true;
+
+            }
+        }
+        public void Get_Saldo()
+        {
+            Console.WriteLine(this.Saldo);
+        }
+        public void Retiro_de_efectivo()
+        {
+            
+        }
+        public void Ingreso_de_efectivo()
+        {
+            
+        }
+
+    }
+}
