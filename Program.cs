eeb7149c5ed8eb4b59e8813e04f72ab5cba7ff2c
Carlos Manuel <Carlos15ros@outlook.es>
2023-08-26 19:34:26 -0600

    dog

--- Program.cs
+++ Program.cs
@@ -4,50 +4,92 @@
     {
         static void Main(string[] args)
         {
-
+            
         }
-        static int pepe(double retiro)
+        static void Rango(double retiro)
         {
             if (retiro > 0 && retiro <= 15)
             {
-                return 0;
+                Billetes_1(retiro);
             }
             else if (retiro > 15 && retiro <= 50)
             {
-                return 1;
+                Billetes_2(retiro);
+                
             }
             else if (retiro > 50 && retiro <= 100)
             {
-                return 2;
+                Billetes_3(retiro);
+                
             }
             else if (retiro > 100)
             {
-                return 3;
+                billetes_4(retiro);
             }
-            else { return 4; }
+            else {}
         }
-        static void Billetes(int i)
+        static void Billetes_1( double retiro)
+        {
+            int Billetes_de_uno = 0;
+            int Billetes_de_cinco = 0;
+
+            Billetes_de_cinco = Convert.ToInt16(((double)Math.Round(retiro * 0.50)) / 5);
+            Billetes_de_uno = Convert.ToInt16((double)(retiro)) - (Billetes_de_cinco * 5);
+
+            Console.WriteLine(Billetes_de_cinco);
+            Console.WriteLine(Billetes_de_uno);
+        }
+        static void Billetes_2( double retiro)
+        {
+            int Billetes_de_uno = 0;
+            int Billetes_de_cinco = 0;
+            int Billetes_de_diez = 0;
+
+            Billetes_de_diez = Convert.ToInt16(((double)Math.Round(retiro * 0.75)) / 10);
+            Billetes_de_cinco = Convert.ToInt16((((double)Math.Round((retiro-Billetes_de_diez*10) * 0.5)) / 5));
+            Billetes_de_uno = Convert.ToInt16((double)(retiro)-((Billetes_de_cinco*5)+(Billetes_de_diez*10)));
+
+
+            Console.WriteLine(Billetes_de_diez);
+            Console.WriteLine(Billetes_de_cinco);
+            Console.WriteLine(Billetes_de_uno);
+        }
+        static void Billetes_3(double retiro)
         {
             int Billetes_de_uno = 0;
             int Billetes_de_cinco = 0;
             int Billetes_de_diez = 0;
             int Billetes_de_veinte = 0;
-            int Billetes_de_cincuanta = 0;
-            switch (i)
-            {
-                case 0:
+
+            Billetes_de_veinte = Convert.ToInt16(((double)Math.Round(retiro * 0.5)) / 20);
+            Billetes_de_diez = Convert.ToInt16(((double)Math.Round((retiro - Billetes_de_veinte * 20) * 0.75)) / 10);
+            Billetes_de_cinco = Convert.ToInt16((((double)Math.Round((retiro - ((Billetes_de_veinte * 20)+(Billetes_de_diez * 10)) * 0.55)) / 5)));
+            Billetes_de_uno = Convert.ToInt16((double)(retiro) - (((Billetes_de_veinte * 20) + (Billetes_de_diez * 10)+(Billetes_de_cinco * 5))));
 
 
+            Console.WriteLine(Billetes_de_veinte);
+            Console.WriteLine(Billetes_de_diez);
+            Console.WriteLine(Billetes_de_cinco);
+            Console.WriteLine(Billetes_de_uno);
+        }
+        static void billetes_4(double retiro)
+        {
+            int Billetes_de_uno = 0;
+            int Billetes_de_cinco = 0;
+            int Billetes_de_diez = 0;
+            int Billetes_de_veinte = 0;
+
+            Billetes_de_veinte = Convert.ToInt16(((double)Math.Round(retiro * 0.5)) / 20);
+            Billetes_de_diez = Convert.ToInt16(((double)Math.Round(retiro  * 0.40)) / 10);
+            Billetes_de_cinco = Convert.ToInt16((((double)Math.Round((retiro * 0.1)) / 5)));
+            Billetes_de_uno = Convert.ToInt16((double)(retiro) - (((Billetes_de_veinte * 20) + (Billetes_de_diez * 10) + (Billetes_de_cinco * 5))));
 
 
-                    break;
-                case 1: break;
-                case 2: break;
-                case 3: break;
-                case 4: break;
-                default: break;
+            Console.WriteLine(Billetes_de_veinte);
+            Console.WriteLine(Billetes_de_diez);
+            Console.WriteLine(Billetes_de_cinco);
+            Console.WriteLine(Billetes_de_uno);
 
-            }
         }
     }
 }
